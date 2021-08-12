0622db3dc2464c2ac777ec4a7d7395d114ab1fc9
itmatter <445948630@qq.com>
2021-08-12 23:49:50 +0800

    播放YUV

---
+++ 12_sdl_play_yuv_video/mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+
+
+void MainWindow::on_playBtn_clicked() {
+    // 调动
+    qDebug() << "on_recordBtn_clicked";
+    if (!_playThread) { // 点击了“播放”
+        qDebug() << "播放";
+        // 开启线程
+        _playThread = new Playthread(this);
+        _playThread->start();
+        connect(_playThread, &Playthread::finished,
+        [this]() { // 线程结束
+            _playThread = nullptr;
+            ui->playBtn->setText("播放");
+        });
+        // 设置按钮文字
+        ui->playBtn->setText("停止");
+    } else { // 点击了“结束录音”
+        // 结束线程
+        qDebug() << "结束";
+        _playThread->setStop(true);
+        _playThread->requestInterruption();
+        _playThread = nullptr;
+        // 设置按钮文字
+        ui->playBtn->setText("播放");
+    }
+}
